57937763aa06aefd2e6a88019c78f4dac5f287e1
Glitch (hello-sqlite) <none>
2022-11-23 07:52:55 -0500

    after interview

--- public/client.js
+++ public/client.js
@@ -25,17 +25,61 @@ const appendNewJob = (label, description) => {
   jobsList.appendChild(newListItem);
 };
 
-const jobHandler = () => {
-  document.getElementById("jobs").classList.toggle("show");
-  // use jobData to populate div passed to click handler
-  jobsList.forEach((job) => {});
+//handler function that helps display the job selected by the user
+const jobSelectionHandler = () => {
+  const dropdowns = document.querySelectorAll(".dropdown");
+  
+  dropdowns.forEach((dropdown) => {
+    //get inner elemnts from each dropdown choice
+    const select = dropdown.querySelector(".select");
+    const selected = dropdown.querySelector(".selected");
+    const options = dropdown.querySelectorAll(".menu li");
+    const menu = dropdown.querySelector(".menu");
+    const caret = dropdown.querySelector(".caret");
+
+    //click event for each selected element
+    select.addEventListener("click", () => {
+      select.classList.toggle("select-slicked");
+      caret.classList.toggle("caret-rotate");
+      menu.classList.toggle("menu-open");
+    });
+
+    options.forEach((option) => {
+      //a click event for the option elements
+      option.addEventListener("click", () => {
+        //selected option will match the inner text
+        selected.innerText = option.innerText;
+
+        select.classList.remove("selected-clicked");
+        caret.classList.remove("caret-rotate");
+
+        //menu will close after user selection
+        menu.classList.remove("menu-open");
+
+        //remove active class from ALL option elements
+        options.forEach((option) => {
+          option.classList.remove("active");
+        });
+
+        option.classList.add("active");
+      });
+    });
+  });
 };
 
+//Dynamically update the job description and location
 const jobSelectedHandler = (id) => {
-  console.log(document.getElementById(id));
+  let selectedJob = document.getElementById(id);
+  let descriptionHeader = document.getElementById("description-header");
+  let jobDescription = document.getElementById("description");
+  let jobLocation = document.getElementById("job-location");
+  
   jobData.forEach((job) => {
     if (id === job.id) {
       console.log(job.description);
+      descriptionHeader.innerText = job.title;
+      jobDescription.innerText = job.description;
+      jobLocation.innerText = job.location;
     }
   });
 };
